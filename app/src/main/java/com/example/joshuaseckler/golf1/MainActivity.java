032a6fe5d778b5e62bb303b125336d3fc638004b
Joshua Seckler <secklerj2013@gmail.com>
2017-07-27 12:57:12 -0400

    added new activity, added color values, things were fucked

--- app/src/main/java/com/example/joshuaseckler/golf1/MainActivity.java
+++ app/src/main/java/com/example/joshuaseckler/golf1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.joshuaseckler.golf1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button firstButton = (Button) findViewById(R.id.button);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //go to different screen
+            }
+        });
     }
+
+    /*@Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }*/
 }
